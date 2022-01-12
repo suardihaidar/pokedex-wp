0ea6af1ee56bff3a49e808d399122420c389780b
Muhammad Suardi <suardihaidar22@gmail.com>
2022-01-12 17:13:24 +0700

    add unit testing for button filter component

---
+++ src/components/ButtonFilter/ButtonFilter.test.tsx
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react";
+import ButtonFilter from "./ButtonFilter";
+
+it("Should render", () => {
+  render(<ButtonFilter onClick={() => {}} type="" title="" />);
+});
