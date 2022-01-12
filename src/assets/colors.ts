b4e6d4a045187bc3af33c1edcc4d5431267f6b78
Muhammad Suardi <suardihaidar22@gmail.com>
2022-01-12 17:12:20 +0700

    add colors asset

---
+++ src/assets/colors.ts
@@ -0,0 +1,24 @@
+const colors: any = {
+    bug: "#C6D16E",
+    dragon: "#A27DFA",
+    fairy: "#F4BDC9",
+    fire: "#F5AC78",
+    ghost: "#A292BC",
+    ground: "#EBD69D",
+    normal: "#C6C6A7",
+    psychic: "#FA92B2",
+    steel: "#D1D1E0",
+    dark: "#A29288",
+    electric: "#FAE078",
+    fighting: "#D67873",
+    flying: "#C6B7F5",
+    grass: "#A7DB8D",
+    ice: "#BCE6E6",
+    poison: "#C183C1",
+    rock: "#D1C17D",
+    water: "#9DB7F5",
+    unknown: "grey",
+    shadow: "grey",
+  };
+
+  export default colors
